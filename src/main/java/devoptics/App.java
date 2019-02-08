880257eb5080298a5b44829689e3841f9abec645
Adam Otto <adamotto89@gmail.com>
2019-02-08 13:54:26 +0100

    Update App.java

--- src/main/java/devoptics/App.java
+++ src/main/java/devoptics/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Beautiful DEMO!" );
+        System.out.println( "Hello DEMO!" );
     }
 }
